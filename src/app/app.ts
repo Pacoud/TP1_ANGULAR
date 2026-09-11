455f95a1709e611fde57e43ddffaafabdd90a421
Pacoud <lucasbolarin@gmail.com>
2026-09-11 09:32:46 +0200

    Ajouts titres avec styles bootstrap(corrigé)

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,6 +9,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   nom_TP = "TP1";
+  resumeSeance = "Prise en main d'Angular, première application";
   protected readonly title = signal('TP1_ANGULAR');
 
 }
---
+++ src/app/etudiant/etudiant.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-etudiant',
+  imports: [],
+  templateUrl: './etudiant.html',
+  styleUrl: './etudiant.css',
+})
+export class Etudiant {}
